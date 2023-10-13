c1ff436d07da04a15c337b7bcd8f3eaba146c79b
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-10-13 23:51:45 +0200

    uses substring(upper(nombre), 0, 1)
    Signed-off-by: plopezgit <p.lopez.ch.hr@hotmail.com>

--- src/queries/t2ExeU.sql
+++ src/queries/t2ExeU.sql
@@ -5,3 +5,4 @@ select nombre, precio as 'Precio en Euro', precio*1.05 as 'Precio en Dolar' from
 select nombre, precio as 'euros', precio*1.05 as 'dolares' from tienda.producto;
 select upper(nombre), precio from tienda.producto;
 select lower(nombre), precio from tienda.producto;
+select nombre, substring(upper(nombre), 0, 1) as 'Extract of name' from tienda.fabricante;
