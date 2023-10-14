037b0f18cf854e7e1f18e2013d1f745ccaca0602
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-10-14 11:07:33 +0200

    uses distinct keyword

--- src/queries/t2ExeU.sql
+++ src/queries/t2ExeU.sql
@@ -7,3 +7,6 @@ select upper(nombre), precio from tienda.producto;
 select lower(nombre), precio from tienda.producto;
 select nombre, substring(upper(nombre), 0, 1) as 'Extract of name' from tienda.fabricante;
 select nombre, format(precio, 2) from tienda.producto;
+select nombre, format(precio, 0) from tienda.producto;
+select codigo_fabricante from tienda.producto;
+select distinct codigo_fabricante from tienda.producto;
